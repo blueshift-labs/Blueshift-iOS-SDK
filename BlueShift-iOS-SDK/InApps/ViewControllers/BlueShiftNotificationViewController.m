028ddc126e7d09a7bebc527eb01b6968818270b8
shahas <shahas@flogic.in>
2019-07-12 20:22:54 +0530

    Base controller and web view controller added

---
+++ BlueShift-iOS-SDK/InApps/ViewControllers/BlueShiftNotificationViewController.m
@@ -0,0 +1,40 @@
+//
+//  BlueShiftNotificationViewController.m
+//  BlueShift-iOS-Extension-SDK
+//
+//  Created by shahas kp on 10/07/19.
+//
+
+#import "BlueShiftNotificationViewController.h"
+
+@interface BlueShiftNotificationViewController ()
+
+@end
+
+@implementation BlueShiftNotificationViewController
+
+- (instancetype)initWithNotification:(BlueShiftInAppNotification *)notification {
+    self = [super init];
+    if (self) {
+        _notification = notification;
+    }
+    return self;
+}
+
+- (void)setTouchesPassThroughWindow:(BOOL) can {
+    self.canTouchesPassThroughWindow = can;
+}
+
+- (void)closeButtonDidTapped {
+    [self hide:YES];
+}
+
+-(void)show:(BOOL)animated {
+    NSAssert(false, @"Override in sub-class");
+}
+
+-(void)hide:(BOOL)animated {
+    NSAssert(false, @"Override in sub-class");
+}
+
+@end
