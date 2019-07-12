8c03fad39e612457af02b59ba4256709a9949d5d
shahas <shahas@flogic.in>
2019-07-12 20:22:20 +0530

    Models required for InAppNotifications

---
+++ BlueShiftWebViewController.m
@@ -0,0 +1,31 @@
+//
+//  BlueShiftWebViewController.m
+//  BlueShift-iOS-Extension-SDK
+//
+//  Created by shahas kp on 10/07/19.
+//
+
+#import "BlueShiftWebViewController.h"
+
+@interface BlueShiftWebViewController ()
+
+@end
+
+@implementation BlueShiftWebViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
