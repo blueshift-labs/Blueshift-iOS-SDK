22c06413b5e23751b6616a13d8d5a25ac6e2244d
Noufal <noufal@flogic.in>
2019-07-19 13:53:59 +0530

    change the payload parsing

--- BlueShift-iOS-SDK/InApps/ViewControllers/BlueShiftNotificationViewController.m
+++ BlueShift-iOS-SDK/InApps/ViewControllers/BlueShiftNotificationViewController.m
@@ -10,6 +10,10 @@
 
 @interface BlueShiftNotificationViewController ()
 
+@property(nonatomic, assign) CGFloat initialHorizontalCenter;
+@property(nonatomic, assign) CGFloat initialTouchPositionX;
+@property(nonatomic, assign) CGFloat originalCenter;
+
 @end
 
 @implementation BlueShiftNotificationViewController
@@ -47,5 +51,63 @@
 -(void)hide:(BOOL)animated {
     NSAssert(false, @"Override in sub-class");
 }
+
+- (CGRect)positionNotificationView:(UIView *)notificationView {
+    CGSize size = CGSizeZero;
+    if ([self.notification.dimensionType  isEqual: @"points"]) {
+        // Ignore Constants.INAPP_X_PERCENT
+        size.width = self.notification.width;
+        size.height = self.notification.height;
+    } else if([self.notification.dimensionType  isEqual: @"percentage"]) {
+        size.width = (CGFloat) ceil([[UIScreen mainScreen] bounds].size.width * (self.notification.width / 100.0f));
+        size.height = (CGFloat) ceil([[UIScreen mainScreen] bounds].size.height * (self.notification.height / 100.0f));
+    }else {
+        
+    }
+    
+    CGRect frame = notificationView.frame;
+    frame.size = size;
+    notificationView.autoresizingMask = UIViewAutoresizingNone;
+    
+    CGSize screenSize = [[UIScreen mainScreen] bounds].size;
+    NSString* position = self.notification.position;
+    
+    if([position  isEqual: INAPP_POSITION_TOP]) {
+        frame.origin.x = (screenSize.width - size.width) / 2.0f;
+        frame.origin.y = 0.0f + 20.0f;
+        notificationView.autoresizingMask = notificationView.autoresizingMask | UIViewAutoresizingFlexibleBottomMargin;
+    } else if([position  isEqual: INAPP_POSITION_CENTER]) {
+        frame.origin.x = (screenSize.width - size.width) / 2.0f;
+        frame.origin.y = (screenSize.height - size.height) / 2.0f;
+    } else if([position  isEqual: INAPP_POSITION_BOTTOM]) {
+        frame.origin.x = (screenSize.width - size.width) / 2.0f;
+        frame.origin.y = screenSize.height - size.height;
+        notificationView.autoresizingMask = notificationView.autoresizingMask | UIViewAutoresizingFlexibleTopMargin;
+    } else {
+        
+    }
+    
+    frame.origin.x = frame.origin.x < 0.0f ? 0.0f : frame.origin.x;
+    frame.origin.y = frame.origin.y < 0.0f ? 0.0f : frame.origin.y;
+    notificationView.frame = frame;
+    _originalCenter = frame.origin.x + frame.size.width / 2.0f;
+    
+    return frame;
+}
+
+- (void)configureBackground {
+    self.view.backgroundColor = [UIColor clearColor];
+}
+
+- (UIColor *)colorWithHexString:(NSString *)str {
+    unsigned char r, g, b;
+    const char *cStr = [str cStringUsingEncoding:NSASCIIStringEncoding];
+    long x = strtol(cStr+1, NULL, 16);
+    b =  x & 0xFF;
+    g = (x >> 8) & 0xFF;
+    r = (x >> 16) & 0xFF;
+    return [UIColor colorWithRed:(float)r/255.0f green:(float)g/255.0f blue:(float)b/255.0f alpha:1];
+}
+
 
 @end
