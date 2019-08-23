34c5ed70456c27c42818a6775fc39191f229e082
Noufal <noufal@flogic.in>
2019-08-23 12:45:32 +0530

    move the app writtern code to sdk

---
+++ BlueShift-iOS-Extension-SDK/BlueshiftInAppEntityAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  BlueshiftInAppEntityAppDelegate.h
+//  BlueShift-iOS-Extension-SDK
+//
+//  Created by Noufal Subair on 23/08/19.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BlueshiftInAppEntityAppDelegate : NSObject
+
+- (void)addInAppNotificationToDataStore:(NSDictionary *)notificationPayload;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BlueShift-iOS-Extension-SDK/InAppNotificationEntity.m
@@ -0,0 +1,224 @@
+//
+//  InAppNotificationEntity.m
+//  BlueShift-iOS-Extension-SDK
+//
+//  Created by shahas kp on 12/07/19.
+//
+
+#import "InAppNotificationEntity.h"
+#import "NSNumber+BlueShiftHelpers.h"
+#import "NSDate+BlueShiftDateHelpers.h"
+#import "BlueShiftNotificationConstants.h"
+
+@implementation InAppNotificationEntity
+
+@dynamic id;
+@dynamic type;
+@dynamic startTime;
+@dynamic endTime;
+@dynamic payload;
+@dynamic priority;
+@dynamic triggerMode;
+@dynamic eventName;
+@dynamic status;
+
+- (void)fetchBy:(NSString *)key withValue:(NSString *)value {
+}
+
+
+
++ (void)fetchAll:(BlueShiftInAppTriggerMode)triggerMode context:(NSManagedObjectContext *)masterContext  withHandler:(void (^)(BOOL, NSArray *))handler {
+    
+    if (nil != masterContext) {
+        
+        NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+        @try {
+            [fetchRequest setEntity:[NSEntityDescription entityForName: kInAppNotificationEntityNameKey inManagedObjectContext: masterContext]];
+        }
+        @catch (NSException *exception) {
+            NSLog(@"Caught exception %@", exception);
+        }
+        if(fetchRequest.entity != nil) {
+            [self fetchFromCoreDataFromContext: masterContext forTriggerMode:triggerMode request: fetchRequest handler: handler];
+        } else {
+            handler(NO, nil);
+        }
+    } else {
+        handler(NO, nil);
+    }
+}
+
++ (void *)fetchFromCoreDataFromContext:(NSManagedObjectContext *)context forTriggerMode: (BlueShiftInAppTriggerMode) triggerMode request: (NSFetchRequest*)fetchRequest handler:(void (^)(BOOL, NSArray *))handler {
+    
+    //NSNumber *currentTimeStamp = [NSNumber numberWithDouble:[[[NSDate date] dateByAddingMinutes:kRequestRetryMinutesInterval] timeIntervalSince1970]];
+    
+    NSString* triggerStr;
+    
+    switch (triggerMode) {
+        case BlueShiftInAppTriggerNow:
+            triggerStr = @"now";
+            break;
+        case BlueShiftInAppTriggerUpComing:
+            triggerStr = @"upcoming";
+            break;
+        case BlueShiftInAppTriggerEvent:
+            triggerStr = @"event";
+            break;
+    }
+    
+    NSPredicate *nextRetryTimeStampLessThanCurrentTimePredicate = [NSPredicate predicateWithFormat:@"triggerMode == %@ AND status == %@", triggerStr, @"ready"];
+    [fetchRequest setPredicate:nextRetryTimeStampLessThanCurrentTimePredicate];
+    
+    @try {
+        if(context && [context isKindOfClass:[NSManagedObjectContext class]]) {
+            [context performBlock:^{
+                NSError *error;
+                NSArray *results = [[NSArray alloc]init];
+                results = [context executeFetchRequest:fetchRequest error:&error];
+                if (results && results.count > 0) {
+                    [self updateNotificationsInQueue:context notifications:results];
+                    handler(YES, results);
+                } else {
+                    handler(NO, nil);
+                }
+            }];
+        } else {
+            handler(NO, nil);
+        }
+    }
+    @catch (NSException *exception) {
+        NSLog(@"Caught exception %@", exception);
+    }
+}
+
+
++ (void)updateNotificationsInQueue:(NSManagedObjectContext *)context notifications:(NSArray *)notifications {
+    for(int i = 0; i < notifications.count; i++) {
+        InAppNotificationEntity *notification = [notifications objectAtIndex:i];
+        
+        //TODO: commented the below code. Dont think its required.
+        //[notification setValue:@"QUEUE" forKey:@"status"];
+    }
+    @try {
+        if(context && [context isKindOfClass:[NSManagedObjectContext class]]) {
+            [context performBlock:^{
+                NSError *error = nil;
+                [context save:&error];
+            }];
+        }
+    }
+    @catch (NSException *exception) {
+        NSLog(@"Caught exception %@", exception);
+    }
+}
+
+
+
+
+- (void) insert:(NSDictionary *)dictionary
+usingPrivateContext: (NSManagedObjectContext*)privateContext
+ andMainContext: (NSManagedObjectContext*)masterContext
+        handler:(void (^)(BOOL))handler {
+    
+    if (nil != masterContext && nil != privateContext) {
+        
+        NSManagedObjectContext *context = privateContext;
+        context.parentContext = masterContext;
+        // return if context is unavailable ...
+        if (context == nil || masterContext == nil) {
+            handler(NO);
+            return;
+        }
+        [self map:dictionary];
+        
+        @try {
+            if(context && [context isKindOfClass:[NSManagedObjectContext class]]) {
+                [context performBlock:^{
+                    NSError *error = nil;
+                    printf("%f InAppNotify:: saving child context ++\n", [[NSDate date] timeIntervalSince1970]);
+                    [context save:&error];
+                    
+                    printf("%f InAppNotify:: saving child context --\n", [[NSDate date] timeIntervalSince1970]);
+                    [context save:&error];
+                    if(masterContext && [masterContext isKindOfClass:[NSManagedObjectContext class]]) {
+                        printf("%f InAppNotify:: masterContext perform block --\n", [[NSDate date] timeIntervalSince1970]);
+                        [masterContext performBlock:^{
+                            NSError *error = nil;
+                            printf("%f InAppNotify:: saving parent context \n", [[NSDate date] timeIntervalSince1970]);
+                            [masterContext save:&error];
+                            handler(YES);
+                        }];
+                    } else {
+                        handler(NO);
+                    }
+                }];
+            } else {
+                handler(NO);
+            }
+        }
+        @catch (NSException *exception) {
+            NSLog(@"Caught exception %@", exception);
+            handler(NO);
+        }
+    } else {
+        handler(NO);
+    }
+}
+
+
+- (void)update:(NSDictionary *)dictionary {
+    
+}
+
+- (void)delete {
+    
+}
+
+- (void)map:(NSDictionary *)dictionary {
+    
+    self.id = [NSString stringWithFormat:@"%u",arc4random_uniform(99999)];
+    
+    NSMutableDictionary *payload = [dictionary mutableCopy];
+    [payload setValue:self.id forKey:@"id"];
+    
+    
+    /* parse the payload and save the relevant keys related to presentation of In-App msg */
+    
+    /* get in-app payload */
+    if ([dictionary objectForKey: kSilentNotificationPayloadIdentifierKey]) {
+        dictionary = [dictionary objectForKey: kSilentNotificationPayloadIdentifierKey];
+    }
+    
+    if ([dictionary objectForKey: kInAppNotificationKey]) {
+         dictionary = [dictionary objectForKey: kInAppNotificationKey];
+    }
+    
+    /* get type of In-App msg */
+    if ([dictionary objectForKey: kSilentNotificationPayloadTypeKey]) {
+        self.type = [dictionary objectForKey: kSilentNotificationPayloadTypeKey];
+    }
+    
+    /* get start and end Time */
+    if ([dictionary objectForKey: kSilentNotificationTriggerEndTimeKey]) {
+        self.endTime = [NSNumber numberWithDouble: [[dictionary objectForKey: kSilentNotificationTriggerEndTimeKey] doubleValue]];
+    }
+    
+    if ([dictionary objectForKey: kSilentNotificationTriggerKey]) {
+        NSDictionary *triggerDictionaryNode = [dictionary objectForKey: kSilentNotificationTriggerKey];
+        if ([triggerDictionaryNode objectForKey:kSilentNotificationTriggerModeKey]) {
+            self.triggerMode = (NSString *)[triggerDictionaryNode objectForKey:kSilentNotificationTriggerModeKey];
+        }
+        if ([triggerDictionaryNode objectForKey:kSilentNotificationTriggerStartTimeKey]) {
+            self.startTime = [NSNumber numberWithDouble: [[triggerDictionaryNode objectForKey:kSilentNotificationTriggerStartTimeKey] doubleValue]];
+        }
+    }
+    
+    /* Other properties */
+    self.priority = @"medium";
+    self.eventName = @"";
+    self.status = @"ready";
+    
+    self.payload = [NSKeyedArchiver archivedDataWithRootObject:payload];
+}
+
+@end
