161a11e99b3cf77ebae128199c084da39fcf4734
noufal <noufal@flogic.in>
2020-01-09 16:00:42 +0530

    release SDK version 2.0.6

--- BlueShift-iOS-Extension-SDK/SDKVersion.h
+++ BlueShift-iOS-Extension-SDK/SDKVersion.h
@@ -8,6 +8,6 @@
 #ifndef SDKVersion_h
 #define SDKVersion_h
 
-#define kSDKVersionNumber   @"2.0.5"
+#define kSDKVersionNumber   @"2.0.6"
 
 #endif /* SDKVersion_h */
--- BlueShift-iOS-SDK/SDKVersion.h
+++ BlueShift-iOS-SDK/SDKVersion.h
@@ -9,6 +9,6 @@
 #ifndef SDKVersion_h
 #define SDKVersion_h
 
-#define kSDKVersionNumber   @"2.0.5"
+#define kSDKVersionNumber   @"2.0.6"
 
 #endif /* SDKVersion_h */
