5de213ac615a8be0e64ecafec12dfe7a7b7d3169
Ketan Shikhare <ketan.shikhare@getblueshift.com>
2021-03-01 19:05:12 +0530

    Release 2.1.14

--- BlueShift-iOS-SDK/SDKVersion.h
+++ BlueShift-iOS-SDK/SDKVersion.h
@@ -9,6 +9,6 @@
 #ifndef SDKVersion_h
 #define SDKVersion_h
 
-#define kSDKVersionNumber   @"2.1.13"
+#define kSDKVersionNumber   @"2.1.14"
 
 #endif /* SDKVersion_h */
--- Package.swift
+++ Package.swift
@@ -6,18 +6,16 @@ import PackageDescription
 let package = Package(
     name: "BlueShift-iOS-SDK",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "BlueShift-iOS-SDK",
-            targets: ["BlueShift-iOS-SDK"]),
+            name: "BlueShift_iOS_SDK",
+            targets: ["BlueShift_iOS_SDK"]),
+        .library(
+            name: "BlueShift_iOS_Extension_SDK",
+            targets: ["BlueShift_iOS_Extension_SDK"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "BlueShift-iOS-SDK", path: "https://github.com/blueshift-labs/Blueshift-iOS-SDK/releases/download/2.1.13/BlueShift_iOS_SDK.xcframework.zip")
+        .binaryTarget(name: "BlueShift_iOS_SDK", url: "https://github.com/blueshift-labs/Blueshift-iOS-SDK/releases/download/2.1.14/BlueShift_iOS_SDK.xcframework.zip", checksum: "658186fcc319bba577a36282ee02998e5f9ecdcffdf8be495ccc13ece44daa55"),
+        .binaryTarget(name: "BlueShift_iOS_Extension_SDK", url: "https://github.com/blueshift-labs/Blueshift-iOS-SDK/releases/download/2.1.14/BlueShift_iOS_Extension_SDK.xcframework.zip", checksum: "ec382d09fac5649bd61cf995ef331cb9c03553d593b472e5b000208bd482a008"),
     ]
 )
