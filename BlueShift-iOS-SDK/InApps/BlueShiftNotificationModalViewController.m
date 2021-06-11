f0bc966b7f41b39b0f50c14777165f9b6c98b5d3
Ketan Shikhare <ketan.shikhare@getblueshift.com>
2021-06-11 10:59:20 +0530

    Added backward compatibility for image modals

--- BlueShift-iOS-SDK/InApps/BlueShiftNotificationModalViewController.m
+++ BlueShift-iOS-SDK/InApps/BlueShiftNotificationModalViewController.m
@@ -444,7 +444,8 @@
     float height = 0;
     
     // Check if this modal is image modal
-    if ([self isBackgroundImagePresentForNotification:self.notification] && (self.notification.templateStyle.width < 0 || self.notification.templateStyle.height < 0)) {
+    if ([self isBackgroundImagePresentForNotification:self.notification]) {
+        //&& (self.notification.templateStyle.width < 0 || self.notification.templateStyle.height < 0)) { //for auto  width & height
         
         float templateWidth = 0;
         if (self.notification.templateStyle && self.notification.templateStyle.width > 0) {
@@ -456,7 +457,7 @@
         }
         // Get max width & height in points which device can support
         float maxWidthInPoints = templateWidth > 0 ? templateWidth : [BlueShiftInAppNotificationHelper convertPercentageWidthToPoints:kInAppNotificationDefaultWidth forWindow:self.window];
-        float maxHeightInPoints = templateHeight > 0 ? templateHeight : [BlueShiftInAppNotificationHelper convertPercentageHeightToPoints: kInAppNotificationDefaultHeight forWindow:self.window];
+        float maxHeightInPoints = [BlueShiftInAppNotificationHelper convertPercentageHeightToPoints: kInAppNotificationImageModalDefaultHeight forWindow:self.window];
         NSData* imageData = [self loadAndCacheImageForURLString:self.notification.templateStyle.backgroundImage];
         UIImage* image = [[UIImage alloc] initWithData:imageData];
         // If image resolution is less than the device height and width, use the image dimention.
