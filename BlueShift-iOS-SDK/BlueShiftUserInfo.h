4f131ea650ac97d7acaebd46820ce2ab6102475c
Ketan Shikhare <ketan.shikhare@getblueshift.com>
2021-06-15 13:27:52 +0530

    set useinfo attributes as nullable

--- BlueShift-iOS-SDK/BlueShiftUserInfo.h
+++ BlueShift-iOS-SDK/BlueShiftUserInfo.h
@@ -12,37 +12,37 @@
 
 /// Set user email id.
 /// Make sure you call BlueShiftUserInfo.sharedInstance()?.save() after setting/modifying any property of user info.
-@property (nonatomic, strong) NSString *email;
+@property (nonatomic, strong) NSString* _Nullable email;
 
 /// Set user customer id.
 /// Make sure you call BlueShiftUserInfo.sharedInstance()?.save() after setting/modifying any property of user info.
-@property (nonatomic, strong) NSString *retailerCustomerID;
+@property (nonatomic, strong) NSString* _Nullable retailerCustomerID;
 
-@property (nonatomic, strong) NSString *name;
-@property (nonatomic, strong) NSString *firstName;
-@property (nonatomic, strong) NSString *lastName;
+@property (nonatomic, strong) NSString* _Nullable name;
+@property (nonatomic, strong) NSString* _Nullable firstName;
+@property (nonatomic, strong) NSString* _Nullable lastName;
 
-@property (nonatomic, strong) NSDate *dateOfBirth;
-@property (nonatomic, strong) NSString *gender;
-@property (nonatomic, strong) NSString *education;
+@property (nonatomic, strong) NSDate* _Nullable dateOfBirth;
+@property (nonatomic, strong) NSString* _Nullable gender;
+@property (nonatomic, strong) NSString* _Nullable education;
 
-@property (nonatomic, strong) NSDate *joinedAt;
+@property (nonatomic, strong) NSDate* _Nullable joinedAt;
 
-@property (nonatomic, strong) NSString *facebookID;
+@property (nonatomic, strong) NSString* _Nullable facebookID;
 
 /// Unsubscribe from the push notifications.
 /// Set this flag to true if you want to stop receiving push notifications for that user.
-@property NSNumber* unsubscribed;
+@property NSNumber* _Nullable  unsubscribed;
 
 /// The data stored in the additionalUserInfo will be populated on server with `additional_user_info__` prefix to every key name.
 /// If key is stored as `profession`, then server will popluate it as `additional_user_info__profession` in the events.
 /// Make sure you call BlueShiftUserInfo.sharedInstance()?.save() after setting/modifying the user info.
-@property NSDictionary *additionalUserInfo;
+@property NSMutableDictionary* _Nullable additionalUserInfo;
 
 /// The data stored in the extras will be sent to server as it is as part of every event.
 /// If key is stored as `profession`, then server will populate it as `profession` in the events.
 /// Make sure you call BlueShiftUserInfo.sharedInstance()?.save() after setting/modifying the user info.
-@property NSDictionary *extras;
+@property NSMutableDictionary* _Nullable extras;
 
 /// Call save method after making any change in the BlueshiftUserInfo data to store the data.
 - (void)save;
@@ -50,10 +50,10 @@
 /// Use this method to erase the stored data.
 + (void)removeCurrentUserInfo;
 
-+ (instancetype) sharedInstance;
++ (instancetype _Nullable) sharedInstance;
 
 /// Returns saved User info as dictionary of key value pairs
-- (NSDictionary *)toDictionary;
+- (NSDictionary *_Nonnull)toDictionary;
 
 @end
 
