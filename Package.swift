ac62502f541f31178b24ff48d790d43fcb1efa08
Ketan Shikhare <ketan.shikhare@getblueshift.com>
2021-09-08 14:54:09 +0530

    Release v2.2.1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "BlueShift_iOS_SDK", url: "https://github.com/blueshift-labs/Blueshift-iOS-SDK/releases/download/2.2.0/BlueShift_iOS_SDK.xcframework.zip", checksum: "a23c33a3929ea9db5bf564c91156228a15ca89eb986bedf102c70a639a45ce90"),
-        .binaryTarget(name: "BlueShift_iOS_Extension_SDK", url: "https://github.com/blueshift-labs/Blueshift-iOS-SDK/releases/download/2.2.0/BlueShift_iOS_Extension_SDK.xcframework.zip", checksum: "0a0e40176b1e7569cc331657e8b26298dab3958e16485d98c60e8a4e22b24f3d"),
+        .binaryTarget(name: "BlueShift_iOS_SDK", url: "https://github.com/blueshift-labs/Blueshift-iOS-SDK/releases/download/2.2.1/BlueShift_iOS_SDK.xcframework.zip", checksum: "5afa48a1cbfde902383601df9a1285b8f16537f3605320e5987c12185dc47af6"),
+        .binaryTarget(name: "BlueShift_iOS_Extension_SDK", url: "https://github.com/blueshift-labs/Blueshift-iOS-SDK/releases/download/2.2.1/BlueShift_iOS_Extension_SDK.xcframework.zip", checksum: "7bf832497c7ddf01daf12a56950624c1abe1f63df017030d93c9206a730a35f1"),
     ]
 )
