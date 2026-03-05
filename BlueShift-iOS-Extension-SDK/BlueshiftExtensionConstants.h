bbe863bec8120efcecbb8083a2078f75ab34375d
vedantpatle <vedant.patle@getblueshift.com>
2026-03-05 12:11:37 +0530

    Version Update

--- BlueShift-iOS-Extension-SDK/BlueshiftExtensionConstants.h
+++ BlueShift-iOS-Extension-SDK/BlueshiftExtensionConstants.h
@@ -47,6 +47,6 @@
 
 #define kAutoUpdateBadge                                                @"auto_update_badge"
 
-#define kBlueshiftExtensionSDKVersion                                   @"2.6.0"
+#define kBlueshiftExtensionSDKVersion                                   @"2.6.1"
 
 #endif
