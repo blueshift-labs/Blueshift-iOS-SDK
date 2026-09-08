c077b91fdf75348120652adf09f9ddeb8fa8e888
vedantpatle <vedant.patle@getblueshift.com>
2026-09-08 12:16:50 +0530

    Bump version to 2.8.1

--- BlueShift-iOS-Extension-SDK/BlueshiftExtensionConstants.h
+++ BlueShift-iOS-Extension-SDK/BlueshiftExtensionConstants.h
@@ -47,6 +47,6 @@
 
 #define kAutoUpdateBadge                                                @"auto_update_badge"
 
-#define kBlueshiftExtensionSDKVersion                                   @"2.8.0"
+#define kBlueshiftExtensionSDKVersion                                   @"2.8.1"
 
 #endif
